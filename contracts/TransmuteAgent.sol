e4dbbdd514196708f660b22b4498c0276dc2a491
David Akers <david@quintify.com>
2017-10-15 01:59:08 -0400

    explicitly declared function visibility, added comments

--- contracts/TransmuteAgent.sol
+++ contracts/TransmuteAgent.sol
@@ -7,7 +7,8 @@ contract TransmuteAgent is Ownable {
 
     /**
     * This contract handles the ethereum 
-    * part of transmuting a token cross-chain
+    * part of transmuting the EIP 20 token 
+    * into Health Cash on Health Nexus
     */
 
     BurnableToken public token;
@@ -15,41 +16,44 @@ contract TransmuteAgent is Ownable {
     uint public transmuteNonce = 0; 
 
     /**
-    The event that will trigger the allocation on 
-    the other chain
+    Used to allocate Health Cash on Health Nexus,
+    required a valid Health Nexus address
     */
     event Transmute(address indexed _from, address indexed _to, uint _value, bytes32 _id);
 
     /*
     * Tokens can not be transmuted without 
-    * first authorizing the transmute agent 
-    * to do so, we need a simple way to 
-    * check if we are authorize to spend the
-    * token for the user. 
+    * first authorizing the TransmuteAgent contract
+    * explicitly. 
     */
     modifier isAuthorizedToSpend(uint _value) {
         assert(authorizedToSpend() >= _value); 
         _;
     }
-    function authorizedToSpend() constant returns (uint) {
+    function authorizedToSpend() public constant returns (uint) {
         return token.allowance(msg.sender, address(this)); 
     }
 
     /**
     * Cross-chain transmution burns the original
-    * tokens. 
+    * HLTH token. 
     */
     function transmuteToken(address _to, uint _value) 
+    public
     isAuthorizedToSpend(_value)
-    returns (bool) 
     {
         require(enabled);
+
+        //first transfer tokens to this contract
         assert(token.transferFrom(msg.sender, address(this), _value));        
+        
+        //burn the tokens
         token.burn(_value);
+
+        //make a record used to allocate on Health Nexus
         transmuteNonce += 1;
         bytes32 id = keccak256(now,transmuteNonce,_to);
         Transmute(msg.sender, _to, _value, id);
-        return true;
     }
 
     /**
